87bf297b16deb0aa40b5a9ae949ba29216c12e54
Evan Mattson <me@aaemnnost.tv>
2016-09-17 03:33:52 +0400

    annotate a few methods on composer

--- src/Composer.php
+++ src/Composer.php
@@ -6,7 +6,9 @@ namespace WP_CLI_Valet;
  * Class Composer
  * @package WP_CLI_Valet
  *
+ * @method static config(...$args)
  * @method static createProject(...$args)
+ * @method static install(...$args)
  * @method static _require(...$args)
  */
 class Composer extends Facade
