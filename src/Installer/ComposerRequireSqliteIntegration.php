bac6e022f3ea54adbf3cbbfee63b01c59df83fa7
Evan Mattson <me@aaemnnost.tv>
2018-09-01 16:16:41 +0300

    require 1.x versions for sqlite db + installer
    + update path to dropin

--- src/Installer/ComposerRequireSqliteIntegration.php
+++ src/Installer/ComposerRequireSqliteIntegration.php
@@ -21,10 +21,10 @@ trait ComposerRequireSqliteIntegration
             'no-interaction' => true,
         ];
 
-        Composer::_require('koodimonni/composer-dropin-installer', $flags);
+        Composer::_require('koodimonni/composer-dropin-installer:^1.0', $flags);
 
-        Composer::config('extra.dropin-paths.web/app/', 'package:aaemnnosttv/wp-sqlite-db:db.php', $flags);
+        Composer::config('extra.dropin-paths.web/app/', 'package:aaemnnosttv/wp-sqlite-db:src/db.php', $flags);
 
-        Composer::_require('aaemnnosttv/wp-sqlite-db:dev-master', $flags);
+        Composer::_require('aaemnnosttv/wp-sqlite-db:^1.0', $flags);
     }
 }
